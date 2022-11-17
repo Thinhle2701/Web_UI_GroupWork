89d3501366017c4c0b44f2b91996ea1f6afa5707
SakLongKa <19520417@gm.uit.edu.vn>
2022-11-17 14:56:45 +0700

    update

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -12,7 +12,7 @@ const Product = ({ product, onAddToCart }) => {
 
   return (
     <Card className={classes.root}>
-      <CardMedia className={classes.media} image={product.image} title={product.name} />
+      <CardMedia className={classes.media} image={"https://i.postimg.cc/VzX3wgR2/g903-lightspeed-wireless-gaming-mouse-3.jpg"} title={product.name} />
       <CardContent>
         <div className={classes.cardContent}>
           <Typography gutterBottom variant="h5" component="h2">
